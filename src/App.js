e3fb98526378b4fa123a667049db5fedf81657f2
eliearja <elie.arja8@gmail.com>
2023-03-05 18:39:39 -0500

    Modification to Skills description

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   const sections = [
     {id: 1, title: "About", description: "A section to get to know me."},
     {id: 2, title: "Work Experience", description: "A section highlighting my work experience."},
-    {id: 3, title: "Skills", description: "A section showcasing my technical and soft skills."},
+    {id: 3, title: "Skills", description: "A section showcasing my skills."},
     {id: 4, title: "Coursework", description: "A section presenting the courses I have taken."}
   ];
 
