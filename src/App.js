3e260f70aba38c156338c1216145f75de44d60b7
eliearja <elie.arja8@gmail.com>
2023-03-09 16:59:38 -0500

    Link Test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   const sections = [
@@ -16,10 +17,12 @@ function App() {
       <div className='GridContainer'>
         <div className='Grid'>
         {sections.map((section) => (
-            <div className='Item' key={section.id}>
+          <Link  className='Item' to={section.path}>
+            <div key={section.id}>
               <h2 className='Title'>{section.title}</h2>
               <strong className='Description'>{section.description}</strong>
             </div>
+          </Link>
         ))}
         </div>
       </div>
